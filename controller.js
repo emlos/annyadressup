b10d794fa4f5e26dd6388bb25e07fc4bc2cff1e6
emlos <slepowronska02@gmail.com>
2025-08-04 01:19:01 +0200

    snapping n stacking n getting clothes by id. next do the deletion gradient

---
+++ controller.js
@@ -0,0 +1,212 @@
+/* ===============================
+         Catalog definition
+      =============================== */
+const catalog = {
+    bras: {
+        title: "Bras",
+        icon: "img/icons/bras.png",
+        items: [
+            {
+                thumb: "img/icons/bras/bra_1_icon.png",
+                image: "img/bras/bra_1.png",
+                },
+            {
+                thumb: "img/icons/bras/bra_2_icon.png",
+                image: "img/bras/bra_2.png",
+                clipPath: "img/bras/bra_2_clipPath.png"
+            }
+        ]
+    },
+    panties: {
+        title: "Panties",
+        icon: "img/icons/panties.png",
+        items: [
+            {
+                thumb: "img/icons/panties/panties_1_icon.png",
+                image: "img/panties/panties_1.png",
+                clipPath: "img/panties/panties_1_clipPath.png"
+            },
+            {
+                thumb: "img/icons/panties/panties_2_icon.png",
+                image: "img/panties/panties_2.png",
+                clipPath: "img/panties/panties_2_clipPath.png"
+            }
+        ]
+    },
+    tops: {
+        title: "Tops",
+        icon: "img/icons/tops.png",
+        items: [
+            {
+                thumb: "img/icons/tops/top_1_icon.png",
+                image: "img/tops/top_1.png",
+                clipPath: "polygon(17.36% 11.55%, 17.20% 13.66%, 16.88% 13.76%, 16.88% 14.50%, 16.56% 14.71%, 16.72% 17.44%, 17.04% 17.44%, 16.88% 18.07%, 18.17% 18.91%, 19.61% 19.43%, 20.26% 19.96%, 21.38% 20.27%, 21.54% 20.59%, 22.51% 20.80%, 22.67% 21.11%, 23.15% 21.22%, 24.44% 22.06%, 24.44% 22.27%, 24.76% 22.27%, 24.92% 22.58%, 25.72% 22.90%, 25.72% 23.11%, 26.05% 23.11%, 27.49% 24.16%, 27.81% 24.16%, 27.97% 24.47%, 28.78% 24.68%, 30.55% 27.10%, 31.03% 27.42%, 31.35% 27.42%, 31.35% 27.73%, 32.48% 28.05%, 32.48% 28.36%, 32.15% 28.36%, 32.15% 28.68%, 31.51% 28.99%, 31.35% 29.41%, 30.39% 30.15%, 30.06% 30.88%, 29.74% 30.99%, 29.74% 31.93%, 29.42% 32.14%, 29.58% 32.35%, 29.26% 32.67%, 29.26% 33.72%, 30.06% 34.24%, 30.23% 34.66%, 30.55% 34.77%, 30.55% 35.08%, 31.03% 35.40%, 33.44% 36.24%, 34.41% 36.34%, 34.41% 36.76%, 34.73% 36.97%, 36.01% 36.76%, 36.50% 36.97%, 36.66% 37.29%, 36.98% 40.13%, 36.82% 40.65%, 37.14% 40.76%, 37.30% 41.91%, 37.46% 42.23%, 37.78% 42.33%, 37.78% 42.65%, 38.59% 42.65%, 38.59% 42.86%, 37.78% 44.12%, 37.30% 44.43%, 36.98% 45.38%, 36.66% 45.48%, 36.66% 45.80%, 36.33% 45.90%, 36.33% 46.22%, 36.01% 46.43%, 36.01% 48.32%, 37.30% 48.42%, 37.62% 48.00%, 38.10% 48.00%, 38.59% 47.69%, 38.59% 47.48%, 39.23% 47.48%, 39.71% 47.16%, 40.35% 47.16%, 41.00% 46.85%, 43.09% 46.43%, 43.57% 46.11%, 46.46% 45.48%, 47.11% 45.27%, 47.11% 45.06%, 47.75% 45.17%, 48.39% 44.85%, 49.68% 44.64%, 49.84% 44.43%, 50.48% 44.43%, 50.96% 44.12%, 51.77% 44.12%, 52.41% 43.80%, 53.86% 43.59%, 54.02% 43.38%, 54.50% 43.38%, 54.82% 43.17%, 55.47% 43.17%, 55.63% 42.96%, 55.95% 42.96%, 55.95% 42.75%, 57.72% 42.75%, 58.68% 43.17%, 60.13% 43.49%, 60.93% 43.49%, 61.25% 43.28%, 62.06% 43.38%, 62.38% 43.07%, 62.22% 42.54%, 61.58% 42.23%, 61.58% 42.02%, 61.09% 41.81%, 60.45% 40.97%, 59.81% 40.65%, 59.81% 40.34%, 60.45% 40.13%, 60.61% 39.81%, 60.93% 39.71%, 61.09% 39.18%, 60.77% 38.87%, 59.49% 38.45%, 59.32% 37.82%, 59.97% 37.82%, 60.29% 37.39%, 61.09% 37.29%, 61.58% 36.87%, 62.70% 36.55%, 62.86% 36.24%, 63.83% 36.13%, 63.99% 35.92%, 64.95% 35.71%, 65.11% 35.50%, 66.40% 35.29%, 66.56% 35.08%, 66.88% 35.08%, 66.88% 34.87%, 67.36% 34.87%, 67.36% 34.66%, 67.85% 34.56%, 68.01% 34.24%, 68.33% 34.24%, 68.33% 34.03%, 68.97% 33.82%, 68.97% 33.61%, 69.29% 33.61%, 69.29% 32.56%, 68.81% 31.83%, 68.81% 31.30%, 68.01% 30.78%, 67.85% 30.25%, 67.52% 30.25%, 67.36% 29.83%, 66.88% 29.62%, 66.88% 29.41%, 66.56% 29.41%, 66.56% 29.20%, 66.08% 28.99%, 66.08% 28.78%, 65.11% 28.15%, 64.95% 27.84%, 65.27% 27.63%, 66.24% 27.63%, 66.88% 27.31%, 67.36% 27.31%, 67.36% 27.10%, 68.01% 26.89%, 68.17% 26.47%, 68.49% 26.47%, 68.97% 25.21%, 69.13% 23.84%, 69.77% 23.53%, 69.77% 23.32%, 70.42% 23.11%, 70.90% 22.37%, 71.22% 22.37%, 71.22% 22.06%, 71.70% 21.95%, 72.67% 20.80%, 72.99% 20.80%, 72.99% 20.59%, 73.47% 20.38%, 73.63% 19.96%, 74.44% 19.43%, 74.76% 18.80%, 75.08% 18.80%, 75.08% 18.49%, 75.40% 18.49%, 75.56% 18.07%, 76.21% 17.65%, 76.21% 17.33%, 76.53% 17.33%, 76.53% 17.02%, 77.17% 16.70%, 77.17% 16.39%, 77.49% 16.39%, 77.81% 15.76%, 78.62% 15.13%, 79.74% 13.34%, 79.74% 12.29%, 80.55% 11.24%, 80.87% 8.40%, 80.39% 8.09%, 79.42% 7.88%, 79.26% 7.67%, 78.30% 7.46%, 78.14% 7.25%, 77.01% 7.04%, 76.69% 6.83%, 76.05% 6.83%, 74.92% 6.41%, 74.12% 6.41%, 73.79% 6.20%, 72.67% 6.20%, 72.35% 5.99%, 71.22% 5.99%, 70.58% 5.78%, 68.49% 5.67%, 66.24% 5.78%, 65.11% 6.20%, 63.83% 6.30%, 63.34% 6.62%, 62.54% 6.62%, 62.22% 6.83%, 61.58% 6.83%, 61.09% 7.04%, 59.81% 7.25%, 59.49% 7.14%, 56.75% 7.67%, 56.75% 7.88%, 56.11% 8.19%, 55.95% 8.72%, 55.63% 8.82%, 55.31% 10.19%, 54.98% 10.29%, 54.98% 11.13%, 55.31% 11.24%, 55.31% 11.55%, 55.79% 11.87%, 55.79% 12.61%, 55.47% 12.71%, 55.14% 13.66%, 55.14% 14.71%, 55.63% 15.44%, 55.95% 15.44%, 56.43% 15.86%, 57.40% 16.07%, 58.04% 16.60%, 58.52% 16.60%, 59.49% 17.12%, 61.09% 17.02%, 61.09% 17.33%, 59.00% 18.17%, 57.07% 18.17%, 56.91% 17.54%, 55.79% 17.54%, 55.79% 17.75%, 55.47% 17.75%, 55.14% 18.17%, 54.82% 18.17%, 54.02% 18.80%, 53.70% 18.80%, 53.22% 19.12%, 53.22% 19.33%, 52.73% 19.43%, 52.73% 19.64%, 52.09% 19.75%, 52.09% 19.96%, 51.77% 20.06%, 49.84% 20.06%, 48.87% 19.85%, 48.71% 19.64%, 48.23% 19.75%, 47.59% 19.43%, 46.95% 19.43%, 46.62% 19.12%, 46.30% 19.22%, 44.69% 18.80%, 44.37% 18.28%, 43.25% 18.28%, 42.77% 20.06%, 42.12% 20.06%, 41.96% 19.75%, 40.68% 19.12%, 40.68% 18.91%, 39.71% 18.70%, 39.71% 18.38%, 40.51% 17.96%, 41.00% 17.12%, 41.32% 17.02%, 41.32% 16.70%, 42.28% 15.97%, 42.12% 15.02%, 41.80% 14.92%, 41.48% 14.29%, 41.16% 14.18%, 41.32% 13.76%, 41.96% 13.34%, 42.12% 11.13%, 40.51% 9.87%, 40.03% 9.87%, 40.03% 9.66%, 39.71% 9.66%, 39.55% 9.45%, 37.30% 8.61%, 37.14% 8.30%, 36.66% 8.30%, 36.33% 7.98%, 33.44% 7.88%, 32.96% 8.09%, 32.32% 8.09%, 31.83% 8.40%, 27.97% 8.82%, 27.81% 9.03%, 23.95% 9.77%, 23.79% 9.98%, 21.86% 10.19%, 19.94% 10.82%, 18.81% 10.92%, 17.36% 11.55%"
+            
+            },
+            {
+                thumb: "img/icons/tops/top_2_icon.png",
+                image: "img/tops/top_2.png",
+                clipPath: "img/tops/top_2_clipPath.png"
+            }
+        ]
+    },
+    bottoms: {
+        icon: "img/icons/tops.png",
+        items: [
+            { image: "img/tops/top_1.png", clipPath: "img/tops/top_1_clipPath.png" },
+            { image: "img/tops/top_2.png", clipPath: "img/tops/top_2_clipPath.png" }
+        ]
+    },
+    outfits: {
+        icon: "img/icons/tops.png",
+        items: [
+            { image: "img/tops/top_1.png", clipPath: "img/tops/top_1_clipPath.png" },
+            { image: "img/tops/top_2.png", clipPath: "img/tops/top_2_clipPath.png" }
+        ]
+    },
+    accessories: {
+        icon: "img/icons/tops.png",
+        items: [
+            { image: "img/tops/top_1.png", clipPath: "img/tops/top_1_clipPath.png" },
+            { image: "img/tops/top_2.png", clipPath: "img/tops/top_2_clipPath.png" }
+        ]
+    }
+};
+
+function init() {
+    buildTabs();
+    buildItems(activeCat);
+}
+
+/* ===============================
+   Build UI
+=============================== */
+const $tabs = $("#tabs");
+const $itemsContainer = $("#itemsContainer");
+var activeCat = null;
+
+function buildTabs() {
+    for (const key in catalog) {
+        const btn = $("<button>").attr("data-cat", key);
+        //alert(btn)
+        btn.append($("<img>").attr("src", catalog[key].icon).attr("alt", key));
+        $tabs.append(btn);
+    }
+    // Activate first tab by default
+    $tabs.find("button").first().addClass("active");
+    activeCat = $tabs.find("button").first().data("cat");
+}
+
+function buildItems(catKey) {
+    $itemsContainer.empty();
+    catalog[catKey].items.forEach((item, idx) => {
+        const id = `${catKey}-${idx}`;
+        const img = $("<img class='item-thumb'>")
+            .attr("src", item.image)
+            .attr("data-id", id)
+            .attr("alt", `${catKey} ${idx}`);
+        $itemsContainer.append(img);
+        makeDraggable(img);
+    });
+}
+
+/* ===============================
+   Drag & Drop logic
+=============================== */
+const $stage = $("#stage");
+const stageRect = () => $stage[0].getBoundingClientRect();
+
+function makeDraggable($el) {
+    $el.draggable({
+        helper: "clone",
+        appendTo: "body",
+        cursor: "grabbing",
+        stop(event, ui) {
+            const dropX = ui.offset.left + ui.helper.width() / 2;
+            const dropY = ui.offset.top + ui.helper.height() / 2;
+            const rect = stageRect();
+
+            // Inside stage?
+            if (dropX > rect.left && dropX < rect.right && dropY > rect.top && dropY < rect.bottom) {
+                placeOnStage($(event.target));
+            }
+        }
+    });
+}
+
+
+
+function placeOnStage($originThumb) {
+    const id = $originThumb.data("id");
+    if ($stage.find(`[data-origin='${id}']`).length) return; // already placed
+
+    const img = createElement($originThumb, id);
+    $stage.append(img);
+
+    $originThumb.addClass("used");
+
+    img.draggable({
+        cursor: "grabbing",
+          stack: ".placed",
+        snap: "#base",
+        snapMode: "inner",
+        snapTolerance: 10,
+        stop(_, ui) {
+            const dropX = ui.offset.left + ui.helper.width() / 2;
+            const dropY = ui.offset.top + ui.helper.height() / 2;
+            const rect = stageRect();
+            console.log(rect)
+            // If moved outside stage → remove & restore thumb
+            if (!(dropX > rect.left && dropX < rect.right && dropY > rect.top && dropY < rect.bottom)) {
+                $originThumb.removeClass("used");
+                img.remove();
+            }
+
+            
+        },
+        drag(event, ui) {
+            const dropX = ui.offset.left + ui.helper.width() / 2;
+            const dropY = ui.offset.top + ui.helper.height() / 2;
+            console.log("dropx", dropX)
+            console.log("dropY", dropY)
+            showGradient();
+        }
+    });
+}
+
+function createElement(origin, id) {
+
+        console.log(id)
+    const type = id.split('-')[0]
+    const itemIndex = id.split('-')[1]
+
+    const item = catalog[type]?.items[itemIndex]
+
+    const img = $("<img class='placed'>")
+        .attr("src", item.image)
+        .attr("data-origin", id)
+        .css("clipPath", item.clipPath)
+
+    
+
+    return img
+}
+
+function showGradient()  {
+
+    const gradient = $("#delete-geadient")
+
+}
+
+/* ===============================
+   Event bindings
+=============================== */
+$tabs.on("click", "button", function () {
+    const $btn = $(this);
+    if ($btn.hasClass("active")) return;
+    $tabs.find("button").removeClass("active");
+    $btn.addClass("active");
+    activeCat = $btn.data("cat");
+    buildItems(activeCat);
+});
+
